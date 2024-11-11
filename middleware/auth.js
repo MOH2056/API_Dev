70eabc663db3f09e833828848f11d7b2fa512b57
MOH2056 <tapereyetunde@gmil.com>
2024-11-11 23:47:05 +0100

    Remove commented-out code for cleaner implementation.

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,4 @@
 import jwt from 'jsonwebtoken'
-//import user from '../model/userModel.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -19,26 +18,5 @@ const auth = (req, res, next) =>{
     }
   )
 }
-
-// const auth = async(req, res, next) => {
-//   const token = req.header('Authorization')?.split(' ')[1];
-//   if (!token) {
-//     return res.status(401).json({ message: 'No token provided' });
-//   }
-//   try {
-//     const decoded = jwt.verify(token, process.env.ACCESS_SECRET);
-//     const User = await user.findOne({
-//       _id: decoded._id
-//     }) 
-//     if (!User)
-//       throw new Error ('Unable to login')
-//     req.User = User
-//     req.token = token
-//     next();
-//   } catch (error) {
-//     res.status(401).json({ message: 'Invalid token' });
-//   }
-// };
-
 
 export default auth;

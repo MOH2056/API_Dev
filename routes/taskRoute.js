3471897af28a3f99bf394e499a72998cc3b5bed4
MOH2056 <tapereyetunde@gmil.com>
2024-11-11 23:49:43 +0100

    Implement authorization middleware in route for access control.

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import auth from '../middleware/auth.js'
+import authorization from '../middleware/authorization.js'
 import { createTask } from '../controller/taskController.js'
 
 const route = express.Router()
 
-route.post('/tasks', auth, createTask)
+route.post('/tasks', auth, authorization(), createTask)
 
 export default route

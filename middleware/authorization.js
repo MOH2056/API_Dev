1b52374b44059479197696544d462e6d0fe901e8
MOH2056 <tapereyetunde@gmil.com>
2024-11-14 17:14:48 +0100

    feat: enhance middleware and update routes for role-based access control
    - Enhanced middleware to ensure secure role-based access control.
    - Updated controllers and routes to use the new authorization logic.

--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,8 +1,30 @@
-const authorization = (requiredRole) => (req, res, next) => {
-    if (req.user.role) return res.status(401).send('Unauthorized access');
+import jwt from 'jsonwebtoken'
 
-    if (req.user.role !== requiredRole) {
-        return res.status(403).send('Access denied: You do not have the required permissions to perform this action.');
+const authorization = (requiredRole) => (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) {
+        return res.status(401).send('Authorization header is missing.');
     }
-    next();
-}
+    console.log(authHeader);
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+        return res.status(401).send('Token missing in authorization header.');
+    }
+    jwt.verify (token, process.env.ACCESS_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send('Invalid or expired token.');
+        }
+        if (!decoded.userrole) {
+            return res.status(401).send('Unauthorized access. Role is missing.');
+        }
+        if (decoded.userrole !== requiredRole) {
+        return res
+            .status(403)
+            .send('Access denied: You do not have the required permissions to perform this action.');
+        }
+        req.user = decoded;
+        next();
+    });
+};
+
+export default authorization;

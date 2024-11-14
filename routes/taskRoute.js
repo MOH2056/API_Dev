f53f4e733dced23b41d108c533d6622e0b51ace9
MOH2056 <tapereyetunde@gmil.com>
2024-11-14 17:15:47 +0100

    feat: add new routes to TaskRoute

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import auth from '../middleware/auth.js'
 import authorization from '../middleware/authorization.js'
-import { createTask } from '../controller/taskController.js'
+import { createTask, getAllTasks, updateTask, getTask, deleteTask, indexSearcher } from '../controller/taskController.js'
 
 const route = express.Router()
 
-route.post('/tasks', auth, authorization(), createTask)
-
+route.post('/tasks', auth, authorization('user'), createTask)
+route.get('/tasks', auth, authorization ('user'), getAllTasks)
+route.get('/tasks/:id', auth, authorization ('user'), getTask)
+route.put('/tasks/:id', auth, authorization ('user'), updateTask)
+route.delete('/tasks/:id', auth, authorization ('user'), deleteTask)
+route.get('/task', auth, authorization ('user'), indexSearcher)
 export default route

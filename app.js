f8de266da2cf9c22091cc28ed5c5d2d571dd19ff
MOH2056 <tapereyetunde@gmil.com>
2024-11-23 15:09:25 +0100

    Add error handler middleware to app.js to handle errors across all routes

--- app.js
+++ app.js
@@ -3,15 +3,24 @@ import express from 'express'
 import connectDB from './db.js'
 import router from './routes/authRoute.js'
 import route from './routes/taskRoute.js'
+import { errorHandler } from './middleware/errorHandler.js'
+import morgan from 'morgan'
+
 dotenv.config
 
 const app = express()
 const PORT = process.env.PORT || 4000
 
 connectDB();
+
+app.use(morgan('tiny'))
+
 app.use(express.json())
+
 app.use('/', router)
 app.use('/', route)
+
+app.use(errorHandler)
 
 app.listen(PORT, (req, res) => {
     console.log(`server is running on ${PORT}....`)
